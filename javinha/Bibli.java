1484cb3c13e8d58b3bb4a458312759833066878c
Henrique Lopes <141880701+useroname23@users.noreply.github.com>
2024-05-10 12:16:05 -0300

    Delete javinha directory

--- javinha/Bibli.java
+++
@@ -1,6 +0,0 @@
-public class Bibli {
-    public static void main(String[] args) {
-        Bibliotecanerson livro = new Bibliotecanerson("josé", "virson", 2040);
-        livro.exibirInfo();
-    }
-}
--- javinha/Conta.java
+++
@@ -1,8 +0,0 @@
-public class Conta {
-    public static void main(String[] args) {
-        ContaBancaria minhaConta = new ContaBancaria(7777, "Maicon jacson");
-        minhaConta.depositar(10000.0);
-        minhaConta.sacar(220.0);
-        minhaConta.exibirInformacoes();
-    }    
-}
